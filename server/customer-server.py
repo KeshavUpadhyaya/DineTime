784cdd19d4a0098fd79242f6abbf96ebf260f32d
Keshav <keshavupadhyayad@gmail.com>
2019-10-05 16:04:20 +0530

    modified requirements

--- server/customer-server.py
+++ server/customer-server.py
@@ -13,6 +13,7 @@ from flask_login import login_user
 from flask_login import logout_user
 from flask_login import login_required
 from flask_login import current_user
+import requests
 
 
 class User(UserMixin):
