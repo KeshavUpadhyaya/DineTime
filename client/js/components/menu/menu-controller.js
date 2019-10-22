afc3f81c61d45f6f59cb6601cdf504fe1ca7109b
Keshav <keshavupadhyayad@gmail.com>
2019-10-22 20:44:31 +0530

    completed menu

--- client/js/components/menu/menu-controller.js
+++ client/js/components/menu/menu-controller.js
@@ -6,11 +6,17 @@ export default class menuController {
 	constructor(dataService, $location) {
 		this.dataService = dataService;
 		this.$location = $location;
+		this.show = 0;
 		console.log("inside menu constructor");
 		this.getMenuItems();
+		this.items = [];
+		if (sessionStorage.items) {
+			this.items = JSON.parse(sessionStorage.items);
+		}
 	}
 
-	format_image_name(str) {
+	formatImageName(str) {
+		//console.log("str = ", str);
 		return str
 			.toLowerCase()
 			.trim()
@@ -22,8 +28,49 @@ export default class menuController {
 		const ctrl = this;
 		this.dataService.getMenuItems().then(function(response) {
 			console.log(response.data);
+			ctrl.data = response.data;
 			ctrl.categories = Object.keys(response.data);
 			console.log(ctrl.categories);
 		});
+	}
+
+	addItem(name, price, quantity) {
+		console.log("quantity = ", quantity);
+		var found = false;
+		for (var i = 0; i < this.items.length && !found; i++) {
+			var item = this.items[i];
+			if (item.name == name) {
+				found = true;
+				item.quantity += quantity;
+				if (item.quantity <= 0 || item.quantity == null) {
+					this.items.splice(i, 1);
+				}
+			}
+		}
+
+		if (!found) {
+			var item = {};
+			item["name"] = name;
+			item["quantity"] = quantity;
+			item["price"] = price;
+			this.items.push(item);
+			//console.log(this.items);
+		}
+
+		sessionStorage.items = JSON.stringify(this.items);
+		console.log(sessionStorage.items);
+	}
+
+	showConfirmOrderPage() {
+		this.$location.path("/confirm-order");
+	}
+
+	getTotalCount() {
+		var count = 0;
+		for (var i = 0; i < this.items.length; i++) {
+			var item = this.items[i];
+			count += item.quantity;
+		}
+		return count;
 	}
 }
