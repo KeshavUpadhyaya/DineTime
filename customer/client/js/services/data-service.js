0ec40f8d488daeacffe377fa2b9cb3d0f412a135
Keshav <keshavupadhyayad@gmail.com>
2019-11-10 19:19:48 +0530

    added few calls to data-service

--- customer/client/js/services/data-service.js
+++ customer/client/js/services/data-service.js
@@ -22,4 +22,54 @@ export default class dataService {
 	getMenuItems() {
 		return this.$http.get(this.ip + "/api/v1/menu");
 	}
+
+	sendOrder(orderId, customerId, takeAway, timeIn, items) {
+		return this.$http.post(this.ip + "/api/v1/order", {
+			order_id: orderId,
+			customer_id: customerId,
+			take_away: takeAway,
+			time_in: timeIn,
+			items: items
+		});
+	}
+
+	getPaymentMethods() {
+		return this.$http.get(this.ip + "/api/v1/payments");
+	}
+
+	sendFavouriteItem(customerId, itemName) {
+		return this.$http.post(this.ip + "/api/v1/setfav", {
+			customer_id: customerId,
+			item_name: itemName
+		});
+	}
+
+	getFavouriteItems(customerId) {
+		return this.$http.get(this.ip + "/api/v1/getfav" + customerId);
+	}
+
+	removeFavoutiteItem(customerId, itemName) {
+		return this.$http.post(this.ip + "/api/v1/delfav", {
+			customer_id: customerId,
+			item_name: itemName
+		});
+	}
+
+	approvePayment(orderId) {
+		return this.$http.post(this.ip + "/api/v1/payment_approval", {
+			order_id: orderId
+		});
+	}
+
+	bookTable(customerId, tableIds, timeIn) {
+		return this.$http.post(this.ip + "/api/v1/booktable", {
+			customer_id: customerId,
+			table_ids: tableIds,
+			time_in: timeIn
+		});
+	}
+
+	getTables() {
+		return this.$http.get(this.ip + "/api/v1/tables");
+	}
 }
