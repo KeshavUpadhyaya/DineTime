444375ef9d53fd3feb523b680f1021c5948db880
kaushik <kaushik@ludakrishna.co.in>
2019-11-20 03:23:20 +0530

    basic waiter

---
+++ waiter/waiter.py
@@ -0,0 +1,25 @@
+from flask import *
+l1=[1, 2, 3]
+
+app = Flask(__name__, template_folder='.') 
+
+@app.route('/')
+@app.route('/index', methods=['GET'])
+def index():
+	req = request.args.get('req')
+	cusid = request.args.get('cusid')
+	tabid = request.args.get('tabid')
+	ordid = request.args.get('ordid')
+	t=(cusid, tabid, ordid)
+	if(req==None):
+		return render_template('./index.html', user='Kaushik', title='Home', nos=l1)
+	#print(req=='-1', t, l1)
+	if(req=='-1'):
+		l1.remove(t)
+	else:
+		l1.append(t)
+	return render_template('./index.html', user='Kaushik', title='Home', nos=l1)
+
+if __name__ == '__main__':
+      app.run(debug=True)
+      #app.run()
